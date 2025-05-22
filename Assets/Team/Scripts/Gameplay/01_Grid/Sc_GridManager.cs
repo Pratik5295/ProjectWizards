7e866bdeeee9156737a981e294b06cb988e899a0
Pratik5295 <36683380+Pratik5295@users.noreply.github.com>
2025-05-22 23:10:38 +0100

    Context Menu functions, Grid System creation logic updated
    - Added context menu options for create and reset grid
    - Rules to not allow multiple grids to be created on top of each other
    - Grid System logic updated to use instantiation as its now happening in context menu
    - Remove at run time grid creation

--- Assets/Team/Scripts/Gameplay/01_Grid/Sc_GridManager.cs
+++ Assets/Team/Scripts/Gameplay/01_Grid/Sc_GridManager.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -13,20 +12,18 @@ public class GridSlot
 public class Sc_GridManager : MonoBehaviour
 {
     [SerializeField] private Vector2 Max_GridSize;
+    [SerializeField] private float gridY = 0.5f;   //Where to spawn the grid wrt height, Default is 0.5f
+    [SerializeField] private float tileSize = 2f; //Width and height of the tile assuming each tile is square
     public Vector2 Max_GridSize_Acc
     {
         get { return Max_GridSize; }
     }
-    [SerializeField] private Vector2 GridSlot_Offset;
-    public Vector2 GridSlot_Offset_Acc
-    {
-        get { return GridSlot_Offset; }
-    }
+    [SerializeField] private float GridSlot_Offset;
 
     [SerializeField] private GameObject BasePrefab;
     [SerializeField] private GameObject HolderPrefab;
 
-
+    [SerializeField]
     private List<GridSlot> Grid = new List<GridSlot>();
     public List<GridSlot> Grid_Acc
     {
@@ -35,52 +32,64 @@ public class Sc_GridManager : MonoBehaviour
 
     private char[] gridCharArray = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J' };
 
-    GameObject ref_gridHolder;
+    [SerializeField]
+    private GameObject ref_gridHolder;
 
-    private void OnEnable()
-    {
-        //CreateGrid();
-    }
-
-    private void Awake()
-    {
-        CreateGrid();
-    }
-
-    public void RefreshGrid()
+    [ContextMenu("Clear Grid")]
+    public void ClearGrid()
     {
         Grid.Clear();
-        Destroy(ref_gridHolder);
-        CreateGrid();
+
+        if (ref_gridHolder == null)
+        {
+            if (transform.childCount > 0)
+            {
+                ref_gridHolder = transform.GetChild(0).gameObject;
+            }       
+        }
+
+        DestroyImmediate(ref_gridHolder);
+
+        ref_gridHolder = null; //Just to avoid the "missing" feedback in inspector
+
     }
 
     //Creates Grid and fills up grid with references to tiles and locations.
-    void CreateGrid()
+    [ContextMenu("Create Grid")]
+    public void CreateGrid()
     {
-        ref_gridHolder = Instantiate(HolderPrefab, new Vector3(0, 0, 0), Quaternion.identity);
-
-        for(int x = 0; x < Max_GridSize.x; x++)
+        if (Grid.Count > 0 || ref_gridHolder != null || transform.childCount > 0)
         {
-            for(int y = 0; y < Max_GridSize.y; y++) //This is the reason why the tiles generate Y before X. Because Y loop completes firstly then moves onto next X axis.
+            Debug.LogError("Grid already exists, destroy the old grid first. No new grid created");
+            return;
+        }
+
+        ref_gridHolder = Instantiate(HolderPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+        ref_gridHolder.transform.SetParent(transform);
+
+        for (int x = 0; x < Max_GridSize.x; x++)
+        {
+            for (int y = 0; y < Max_GridSize.y; y++)
             {
-                GridSlot newSlot = new GridSlot();
-                newSlot.GridID = new Vector2(x, y);
-                if(BasePrefab != null)
+                float positionX = x * (tileSize + GridSlot_Offset);
+                float positionY = y * (tileSize + GridSlot_Offset);
+
+                GridSlot newSlot = new GridSlot
                 {
-                    newSlot.tilePrefab = BasePrefab;
-                }
+                    GridID = new Vector2(x, y),
+                    tilePrefab = BasePrefab
+                };
                 Grid.Add(newSlot);
 
-                Collider[] grabbedWorldTiles = Physics.OverlapSphere(new Vector3(newSlot.GridID.x, 0, newSlot.GridID.y), 0.5f);
-                GameObject selectedTile = grabbedWorldTiles[0].gameObject;
-                newSlot.tilePrefab = selectedTile;
+                var spawnedTile = SpawnTile(positionX, gridY, positionY);
+                newSlot.tilePrefab = spawnedTile;
                 newSlot.tilePrefab.name = $"Tile: {gridCharArray[x]} , {y}";
-
-                /*var spawnedTile = Instantiate(selectedTile, 
-                    new Vector3(newSlot.GridID.x * GridSlot_Offset.x, 10, newSlot.GridID.y * GridSlot_Offset.y), Quaternion.identity, ref_gridHolder.transform);
-
-                spawnedTile.name = $"Tile: {gridCharArray[x]} , {y}";*/
             }
         }
+    }
+
+    private GameObject SpawnTile(float x, float y, float z)
+    {
+        return Instantiate(BasePrefab,new Vector3(x,y, z), Quaternion.identity, ref_gridHolder.transform);
     }
 }
