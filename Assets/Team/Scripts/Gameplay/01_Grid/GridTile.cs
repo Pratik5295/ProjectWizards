ccfe4af9dc224a8c514634efa07c3e22c800eb96
PRATIK SHRINGARPURE <36683380+Pratik5295@users.noreply.github.com>
2025-05-23 11:26:58 +0100

    Grid Tile system and update on grid implementation

---
+++ Assets/Team/Scripts/Gameplay/01_Grid/GridTile.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+
+namespace Team.Gameplay.GridSystem
+{
+    public enum TileType
+    {
+        EMPTY = 0, //Highlights no tile
+        TILE = 1 //Tile has content (not an empty tile)
+    }
+
+    public enum TileFacing
+    {
+        NORTH = 0,
+        SOUTH = 1,
+        EAST = 2,   
+        WEST = 3
+    }
+
+    [System.Serializable]
+    public class TileDirection
+    {
+        public TileFacing TileFacing;
+    }
+
+    public class GridTile : MonoBehaviour
+    {
+        public Vector2 GridID; // Position . TODO: Update it to highlight position properly in vector?
+
+        public GameObject tilePrefab;
+
+        public TileType tileType;
+
+        public TileDirection Direction; //Rotation 
+
+        [SerializeField]
+        private Sc_GridManager gridManager;
+
+        [SerializeField]
+        private GameObject tileObject = null; //The created tile object
+
+        /// <summary>
+        /// Initialize the tile
+        /// </summary>
+        public bool Init(Sc_GridManager _gridManager)
+        {
+            gridManager = _gridManager;
+
+            //Check if spawn tile
+            if (ShouldSpawnTile())
+            {
+                tileObject = SpawnTileObject();
+
+                //Setup each tile to be facing north at start
+                Direction = new TileDirection
+                {
+                    TileFacing = TileFacing.NORTH
+                };
+
+
+                return true;
+            }
+
+            return false;
+        }
+
+        private GameObject SpawnTileObject()
+        {
+            return Instantiate(tilePrefab, transform);
+        }
+
+        private bool ShouldSpawnTile()
+        {
+            return tileType == TileType.TILE;
+        }
+
+        [ContextMenu("Set Tile Empty")]
+        public void SetTileEmpty()
+        {
+            tileType = TileType.EMPTY;
+            DestroyImmediate(tileObject);
+            tileObject = null;
+            gridManager?.RemoveTileFromGrid(this);
+        }
+
+        [ContextMenu("Set Tile to Object")]
+        public void SetTileObject()
+        {
+            tileType = TileType.TILE;
+            SpawnTileObject();
+            gridManager?.AddTileToGrid(this);
+        }
+    }
+}
--- Assets/Team/Scripts/Gameplay/01_Grid/Sc_GridManager.cs
+++ Assets/Team/Scripts/Gameplay/01_Grid/Sc_GridManager.cs
@@ -1,95 +1,131 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GridSlot
+namespace Team.Gameplay.GridSystem
 {
-    public Vector2 GridID;
 
-    public GameObject tilePrefab;
-
-}
-
-public class Sc_GridManager : MonoBehaviour
-{
-    [SerializeField] private Vector2 Max_GridSize;
-    [SerializeField] private float gridY = 0.5f;   //Where to spawn the grid wrt height, Default is 0.5f
-    [SerializeField] private float tileSize = 2f; //Width and height of the tile assuming each tile is square
-    public Vector2 Max_GridSize_Acc
+    public class Sc_GridManager : MonoBehaviour
     {
-        get { return Max_GridSize; }
-    }
-    [SerializeField] private float GridSlot_Offset;
-
-    [SerializeField] private GameObject BasePrefab;
-    [SerializeField] private GameObject HolderPrefab;
-
-    [SerializeField]
-    private List<GridSlot> Grid = new List<GridSlot>();
-    public List<GridSlot> Grid_Acc
-    {
-        get{return Grid; }
-    }
-
-    private char[] gridCharArray = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J' };
-
-    [SerializeField]
-    private GameObject ref_gridHolder;
-
-    [ContextMenu("Clear Grid")]
-    public void ClearGrid()
-    {
-        Grid.Clear();
-
-        if (ref_gridHolder == null)
+        [SerializeField] private Vector2 Max_GridSize;
+        [SerializeField] private float gridY = 0.5f;   //Where to spawn the grid wrt height, Default is 0.5f
+        [SerializeField] private float tileSize = 2f; //Width and height of the tile assuming each tile is square
+        public Vector2 Max_GridSize_Acc
         {
-            if (transform.childCount > 0)
-            {
-                ref_gridHolder = transform.GetChild(0).gameObject;
-            }       
+            get { return Max_GridSize; }
+        }
+        [SerializeField] private float GridSlot_Offset;
+
+        [SerializeField] private GameObject HolderPrefab;
+
+        [SerializeField]
+        private List<GameObject> TileMap = new List<GameObject>();
+
+        [SerializeField]
+        private List<GridTile> Grid = new List<GridTile>();
+        public List<GridTile> Grid_Acc
+        {
+            get { return Grid; }
         }
 
-        DestroyImmediate(ref_gridHolder);
+        private char[] gridCharArray = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J' };
 
-        ref_gridHolder = null; //Just to avoid the "missing" feedback in inspector
+        [SerializeField]
+        private GameObject ref_gridHolder;
 
-    }
-
-    //Creates Grid and fills up grid with references to tiles and locations.
-    [ContextMenu("Create Grid")]
-    public void CreateGrid()
-    {
-        if (Grid.Count > 0 || ref_gridHolder != null || transform.childCount > 0)
+        [ContextMenu("Clear Grid")]
+        public void ClearGrid()
         {
-            Debug.LogError("Grid already exists, destroy the old grid first. No new grid created");
-            return;
-        }
+            Grid.Clear();
 
-        ref_gridHolder = Instantiate(HolderPrefab, new Vector3(0, 0, 0), Quaternion.identity);
-        ref_gridHolder.transform.SetParent(transform);
-
-        for (int x = 0; x < Max_GridSize.x; x++)
-        {
-            for (int y = 0; y < Max_GridSize.y; y++)
+            if (ref_gridHolder == null)
             {
-                float positionX = x * (tileSize + GridSlot_Offset);
-                float positionY = y * (tileSize + GridSlot_Offset);
-
-                GridSlot newSlot = new GridSlot
+                if (transform.childCount > 0)
                 {
-                    GridID = new Vector2(x, y),
-                    tilePrefab = BasePrefab
-                };
-                Grid.Add(newSlot);
+                    ref_gridHolder = transform.GetChild(0).gameObject;
+                }
+            }
 
-                var spawnedTile = SpawnTile(positionX, gridY, positionY);
-                newSlot.tilePrefab = spawnedTile;
-                newSlot.tilePrefab.name = $"Tile: {gridCharArray[x]} , {y}";
+            DestroyImmediate(ref_gridHolder);
+
+            ref_gridHolder = null; //Just to avoid the "missing" feedback in inspector
+
+        }
+
+        //Creates Grid and fills up grid with references to tiles and locations.
+        [ContextMenu("Create Grid")]
+        public void CreateGrid()
+        {
+            if (Grid.Count > 0 || ref_gridHolder != null || transform.childCount > 0)
+            {
+                Debug.LogError("Grid already exists, destroy the old grid first. No new grid created");
+                return;
+            }
+
+            ref_gridHolder = Instantiate(HolderPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+            ref_gridHolder.transform.SetParent(transform);
+
+            for (int x = 0; x < Max_GridSize.x; x++)
+            {
+                for (int y = 0; y < Max_GridSize.y; y++)
+                {
+                    float positionX = x * (tileSize + GridSlot_Offset);
+                    float positionY = y * (tileSize + GridSlot_Offset);
+
+                    GridTile newSlot = new GridTile
+                    {
+                        GridID = new Vector2(x, y)
+                    };
+
+                    //Find the random tile
+                    var tileToSpawn = GetRandomTile();
+
+                    var spawnedTile = SpawnTile(tileToSpawn,positionX, gridY, positionY);
+                    var gridTile = spawnedTile.GetComponent<GridTile>();
+                    bool isWalkable = gridTile.Init(this); //Update this to look cleaner and error check
+
+                    newSlot.tilePrefab = spawnedTile;
+                    newSlot.tilePrefab.name = $"Tile: {gridCharArray[x]} , {y}";
+
+                    if (isWalkable)
+                    {
+                        Grid.Add(newSlot);
+                    }
+                }
             }
         }
-    }
 
-    private GameObject SpawnTile(float x, float y, float z)
-    {
-        return Instantiate(BasePrefab,new Vector3(x,y, z), Quaternion.identity, ref_gridHolder.transform);
+        private GameObject SpawnTile(GameObject _tile,float x, float y, float z)
+        {
+            return Instantiate(_tile, new Vector3(x, y, z), Quaternion.identity, ref_gridHolder.transform);
+        }
+
+        /// <summary>
+        /// Returns a random GameObject from the gameTiles list.
+        /// </summary>
+        public GameObject GetRandomTile()
+        {
+            if (TileMap == null || TileMap.Count == 0)
+            {
+                Debug.LogWarning("Game tiles list is empty or not assigned.");
+                return null;
+            }
+
+            int randomIndex = Random.Range(0, TileMap.Count);
+            return TileMap[randomIndex];
+        }
+
+
+        //Helpers for grid creating
+        public void RemoveTileFromGrid(GridTile _tile)
+        {
+            if (!Grid.Contains(_tile)) return;
+            Grid.Remove(_tile);
+        }
+
+        public void AddTileToGrid(GridTile _tile)
+        {
+            if (Grid.Contains(_tile)) return;
+            Grid.Add(_tile);
+        }
     }
 }
