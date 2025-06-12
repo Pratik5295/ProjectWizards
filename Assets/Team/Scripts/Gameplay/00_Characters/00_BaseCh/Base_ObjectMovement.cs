b658c8954b8c0dda3dafc8e0c4ca189ddf6a75d2
Pratik5295 <36683380+Pratik5295@users.noreply.github.com>
2025-06-12 20:02:24 +0100

    Obstacle data gets value for Object Movement

--- Assets/Team/Scripts/Gameplay/00_Characters/00_BaseCh/Base_ObjectMovement.cs
+++ Assets/Team/Scripts/Gameplay/00_Characters/00_BaseCh/Base_ObjectMovement.cs
@@ -3,8 +3,11 @@ using UnityEngine;
 public class Base_ObjectMovement : Base_Ch
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField]
+    private ObstacleData _obstacleData;
     void Start()
     {
-        InitialiseCharacter(_currentTileID, Enum_GridDirection.NORTH);
+        _obstacleData = GetComponent<ObstacleData>();
+        InitialiseCharacter(_obstacleData.CurrentTileID, Enum_GridDirection.NORTH);
     }
 }
