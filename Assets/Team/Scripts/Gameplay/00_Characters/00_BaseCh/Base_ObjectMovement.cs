800123fc35580074ae43be8a117010a818868672
PRATIK SHRINGARPURE <36683380+Pratik5295@users.noreply.github.com>
2025-06-13 09:59:23 +0100

    Double obstacle data variables from main merge, removed

--- Assets/Team/Scripts/Gameplay/00_Characters/00_BaseCh/Base_ObjectMovement.cs
+++ Assets/Team/Scripts/Gameplay/00_Characters/00_BaseCh/Base_ObjectMovement.cs
@@ -1,13 +1,10 @@
-using Team.Gameplay.GridSystem;
 using UnityEngine;
 
 public class Base_ObjectMovement : Base_Ch
 {
     [SerializeField]
     private ObstacleData _obstacleData;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    [SerializeField]
-    private ObstacleData _obstacleData;
+
     void Start()
     {
         _obstacleData = GetComponent<ObstacleData>();   
