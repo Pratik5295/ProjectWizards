a179c97d092afd06df8712027e2c9aeb02bc48e4
Yakovafox <99608146+Yakovafox@users.noreply.github.com>
2025-06-16 09:54:47 +0100

    Implemented Redirect Wizard
    ---Redirect Wizard---
    + Implemented code for Redirect wizard.
    + Created Redirect wizard prefab that is setup and ready to be used.
    + Wizard is a version of the projectile wizard with extra code, that waits to absorb a projectile. On their turn they can then shoot that projectile out in a different direction.
    
    ---Other---
    + Created new scene for testing the redirect wizard, with the UI and other developed systems.

--- Assets/Team/Scripts/Gameplay/05_Projectiles/FireballProjectile.cs
+++ Assets/Team/Scripts/Gameplay/05_Projectiles/FireballProjectile.cs
@@ -9,6 +9,11 @@ public class FireballProjectile : Base_Projectile
     public override void OnTriggerEnter(Collider other)
     {
         if(other.gameObject == CastingWizard || other.gameObject.layer == 3) { return; } //Check that the collision isnt with the wizard that casted the projectile.
+        if (other.GetComponent<ChRedirectWizard>())
+        {
+            base.OnTriggerEnter(other);
+            return;
+        }
 
         if (other.CompareTag(MetaConstants.CharacterTag))
         {
