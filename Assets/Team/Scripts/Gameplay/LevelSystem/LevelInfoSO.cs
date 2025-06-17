3109b05c5253e69f1c08ac22c8b284a5f541db70
PRATIK SHRINGARPURE <36683380+Pratik5295@users.noreply.github.com>
2025-06-17 13:16:34 +0100

    Level Info SO and Level Info Data class

---
+++ Assets/Team/Scripts/Gameplay/LevelSystem/LevelInfoSO.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using static Team.GameConstants.MetaConstants;
+
+namespace Team.GameConstants
+{
+    public static partial class MetaConstants
+    {
+        public enum LevelState
+        {
+            LOCKED = 0,
+            UNLOCKED = 1
+        }
+    }
+}
+
+namespace Team.Gameplay.LevelSystem
+{
+    
+
+    [System.Serializable]
+    public class LevelInfoData
+    {
+        public LevelState State;
+        public LevelInfoData Requirements;
+        public bool IsCompleted()
+        {
+            if(Requirements == null)
+            {
+                return true;
+            }
+
+            return Requirements.IsCompleted();
+        }
+    }
+
+
+    [CreateAssetMenu(fileName = "LevelInfoSO",menuName = "Team/Data/Levels/Create a Level Info File")]
+    public class LevelInfoSO : ScriptableObject
+    {
+        public LevelInfoData Data;
+        public GameObject GameLevelPrefab;
+    }
+}
