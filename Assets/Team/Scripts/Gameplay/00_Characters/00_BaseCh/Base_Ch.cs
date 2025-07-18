2cc3fe4c67e347ad7279f879004f5b15f5b0caf9
Yakovafox <99608146+Yakovafox@users.noreply.github.com>
2025-07-18 14:05:35 +0100

    Fixing push breaking game when not hitting something or character cannot move
    + also implemented eventReference to levelDataSO.

--- Assets/Team/Scripts/Gameplay/00_Characters/00_BaseCh/Base_Ch.cs
+++ Assets/Team/Scripts/Gameplay/00_Characters/00_BaseCh/Base_Ch.cs
@@ -249,6 +249,11 @@ public class Base_Ch : MonoBehaviour, IMoveable, IProjectileHittable, IUsableAbi
             {
                 StartCoroutine(ShakeCharacter(0.25f));
                 alreadyMoving = false;
+                if (wasPushed)
+                {
+                    WasPushed(wasPushed);
+                    yield break;
+                }
                 OnTurnComplete?.Invoke();
                 yield break;
             }
@@ -262,11 +267,7 @@ public class Base_Ch : MonoBehaviour, IMoveable, IProjectileHittable, IUsableAbi
 
         if (wasPushed)
         {
-            if (PushProjectileInstance)
-            {
-                PushProjectileInstance.CleanUp();
-                PushProjectileInstance = null;
-            }
+            WasPushed(wasPushed);
             yield break;
         }
 
@@ -386,6 +387,18 @@ public class Base_Ch : MonoBehaviour, IMoveable, IProjectileHittable, IUsableAbi
             yield return null;
         }
         transform.localPosition = defaultPos;
+    }
+
+    private void WasPushed(bool wasPushed)
+    {
+        if (wasPushed)
+        {
+            if (PushProjectileInstance)
+            {
+                PushProjectileInstance.CleanUp();
+                PushProjectileInstance = null;
+            }
+        }
     }
 
     public void UpdateCurrentTileID()
