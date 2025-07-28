cd426c77750cae428f9fe684607a9a7ddf49bc77
Yakovafox <99608146+Yakovafox@users.noreply.github.com>
2025-07-28 17:38:03 +0100

    Game Fail Feedback, Explosive Barrel Anim, oil tile colour fix + Rocket
    + Implemented a fail effect for when players fail the level, the screen shakes and an not yet implemented audio will play.
    + Fixing visual bugs with the oil tiles colour getting changed and stuck to a darker colour, after rotation.
    + Implemented an explosive barrel animation to emphasise the explosion more.
    + Creating a Racoon.
    + Implementing a Racoon and means of its functionality.
    + Rockets code is all there, they are missing their model though.

--- Assets/Team/Scripts/Gameplay/01_Grid/GridManager.cs
+++ Assets/Team/Scripts/Gameplay/01_Grid/GridManager.cs
@@ -162,6 +162,8 @@ namespace Team.Gameplay.GridSystem
             Instantiate(_fireManager, Vector3.zero, Quaternion.identity, transform.parent);
             Instantiate(_projectileManager, Vector3.zero, Quaternion.identity, transform.parent);
             Instantiate(_ClickableManager, Vector3.zero, Quaternion.identity, transform.parent);
+
+            EnvrionmentalsManager.Instance.ExecuteRocketsSpawnLoop();
         }
     }
 }
